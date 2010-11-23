e4f66d37a8466f0cce9cfe7d66ade84a7e2a287a
Jonas Baumann <jone@jone.ch>
2010-11-23 15:23:14 +0100

    renamed README.txt to README.rst - give github a hint

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0a1'
 setup(name='collective.dexteritytextindexer',
       version=version,
       description="Dynamic SearchableText index for dexterity content types",
-      long_description=open("README.txt").read() + "\n" + \
+      long_description=open("README.rst").read() + "\n" + \
           open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
