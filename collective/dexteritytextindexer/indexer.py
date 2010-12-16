673c3013a69685ac48f97cf25324b87b3db6ad37
Jonas Baumann <jone@jone.ch>
2010-12-16 13:22:50 +0100

    changed the indexer to using converter adapters

--- collective/dexteritytextindexer/indexer.py
+++ collective/dexteritytextindexer/indexer.py
@@ -1,19 +1,17 @@
-from Products.CMFCore.utils import getToolByName
-from ZODB.POSException import ConflictError
+from collective.dexteritytextindexer import interfaces
 from collective.dexteritytextindexer.behavior import IDexterityTextIndexer
 from collective.dexteritytextindexer.directives import SEARCHABLE_KEY
 from five import grok
 from plone.dexterity.utils import iterSchemata
 from plone.indexer import indexer
-from plone.namedfile.interfaces import INamedFileField
+from plone.z3cform import z2
+from z3c.form.field import Field
+from z3c.form.interfaces import DISPLAY_MODE, IFieldWidget
+from z3c.form.interfaces import IContextAware, IFormLayer, IField
 from zope import schema
-from zope.app.component.hooks import getSite
-from zope.component import getAdapters
+from zope.component import getAdapters, getMultiAdapter
 from zope.interface import Interface
-import logging
-
-
-LOGGER = logging.getLogger('collective.dexteritytextindexer')
+from zope.interface import alsoProvides
 
 
 class IDynamicTextIndexExtender(Interface):
@@ -27,49 +25,96 @@ def dynamic_searchable_text_indexer(obj):
     """Dynamic searchable text indexer.
     """
 
+    # We need to make sure that we have z2 moe switched on for z3c form.
+    # Since we do not really have any view to do this on, we just use
+    # the "view" view, which usually exists. It's not necessary that
+    # the view points to a z3cform, only view.request is used..
+    view = obj.restrictedTraverse('view')
+    z2.switch_on(view, request_layer=IFormLayer)
+
     indexed = []
 
     for storage, fields in get_searchable_contexts_and_fields(obj):
         for field in fields:
-            value = field.get(storage)
+
+            # we need the form-field, not the schema-field we
+            # already have..
+            form_field = Field(field, interface=field.interface,
+                       prefix='')
+
+            # get the widget
+            widget = get_field_widget(obj, form_field)
+
+            # get the converter for this field / widget
+            converter = getMultiAdapter(
+                (obj, field, widget),
+                interfaces.IDexterityTextIndexFieldConverter)
+
+            # convert the field value
+            value = converter.convert()
+
+            # if no value was returned, we don't need to index
+            # anything.
             if not value:
                 continue
 
-            if INamedFileField.providedBy(field):
-                value = get_transformed_file(value)
+            # be sure that it is utf-8 encoded
+            if isinstance(value, unicode):
+                value = value.encode('utf-8')
 
-            if isinstance(value, tuple) or isinstance(value, list):
-                for subval in value:
-                    if isinstance(subval, unicode):
-                        subval = subval.encode('utf-8')
-                    indexed.append(subval)
+            # only accept strings
+            assert isinstance(value, str), 'expected converted ' + \
+                'value of IDexterityTextIndexFieldConverter to be a str'
 
-            elif isinstance(value, dict):
-                for subval in value.values():
-                    if isinstance(subval, unicode):
-                        subval = subval.encode('utf-8')
-                    indexed.append(subval)
+            indexed.append(value)
 
-            elif isinstance(value, unicode):
-                indexed.append(value.encode('utf-8'))
-
-            elif isinstance(value, str):
-                indexed.append(value)
-
-            elif value:
-                indexed.append(str(value))
-
-    for name, adapter in getAdapters((obj, ), IDynamicTextIndexExtender):
+    # after converting all fields, run additional
+    # IDynamicTextIndexExtender adapters.
+    for name, adapter in getAdapters((obj,), IDynamicTextIndexExtender):
         extended_value = adapter()
+
+        # if no value was returned, we don't need to index anything.
         if not extended_value:
             continue
+
+        # be sure that it is utf-8 encoded
         if isinstance(extended_value, unicode):
             extended_value = extended_value.encode('utf-8')
+
+        # only accept strings
+        assert isinstance(value, str), 'expected converted ' + \
+            'value of IDynamicTextIndexExtender to be a str'
+
         indexed.append(extended_value)
 
     return ' '.join(indexed)
 
-grok.global_adapter(dynamic_searchable_text_indexer, name='SearchableText')
+
+grok.global_adapter(dynamic_searchable_text_indexer,
+                    name='SearchableText')
+
+
+def get_field_widget(obj, field):
+    """Returns the field widget of a field in display mode without
+    touching any form.
+    The `field` should be a z3c form field, not a zope schema field.
+    """
+
+    assert IField.providedBy(field), 'field is not a form field'
+
+    if field.widgetFactory.get(DISPLAY_MODE) is not None:
+        factory = field.widgetFactory.get(DISPLAY_MODE)
+        widget = factory(field.field, obj.REQUEST)
+    else:
+        widget = getMultiAdapter(
+            (field.field, obj.REQUEST), IFieldWidget)
+    widget.name = '' + field.__name__  # prefix not needed
+    widget.id = widget.name.replace('.', '-')
+    widget.context = obj
+    alsoProvides(widget, IContextAware)
+    widget.mode = DISPLAY_MODE
+    widget.update()
+    return widget
 
 
 def get_searchable_contexts_and_fields(obj):
@@ -90,22 +135,3 @@ def get_searchable_contexts_and_fields(obj):
         if fields:
             storage = schemata(obj)
             yield storage, fields
-
-
-def get_transformed_file(data):
-    """Transforms field data to text for indexing safely.
-    """
-    transforms = getToolByName(getSite(), 'portal_transforms')
-
-    if transforms._findPath(data.contentType, 'text/plain'):
-        return ''
-
-    try:
-        return transforms.convertTo(
-            'text/plain', data.data, mimetype=data.contentType,
-            filename=data.filename)
-    except (ConflictError, KeyboardInterrupt):
-        raise
-    except Exception, e:
-        LOGGER.error('Error while trying to convert file contents '
-                     'to "text/plain": %s' % str(e))
