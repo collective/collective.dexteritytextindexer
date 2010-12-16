6149507c8b7bf3c09134f28f4fd82af6b0b5f969
Jonas Baumann <jone@jone.ch>
2010-12-16 10:20:31 +0100

    added field converter interface

---
+++ collective/dexteritytextindexer/interfaces.py
@@ -0,0 +1,18 @@
+from zope.interface import Interface
+
+
+class IDexterityTextIndexFieldConverter(Interface):
+    """Interface for a multi-adapter which converts the field value of the
+    adapted field into a human readable, translated text for indexing in
+    the searchable text index.
+    """
+
+    def __init__(context, field, widget):
+        """The multi-adpater adapts the context, the field and the widget.
+        """
+
+    def convert():
+        """Returns a string containing the words to index. Translatable
+        Message-objects are already translated into normal strings. On a
+        multi-language site the
+        """
