0325369092df66485d9dbad66d5e2c6cc7fd22ec
Jonas Baumann <jone@jone.ch>
2010-12-16 13:18:19 +0100

    added NamedFile converter, but add it optionally, so that plone.namedfile is not required any more

--- collective/dexteritytextindexer/converters.py
+++ collective/dexteritytextindexer/converters.py
@@ -1,4 +1,5 @@
 from Products.CMFCore.utils import getToolByName
+from ZODB.POSException import ConflictError
 from collective.dexteritytextindexer import interfaces
 from five import grok
 from plone.dexterity.interfaces import IDexterityContent
@@ -36,3 +37,40 @@ class DefaultDexterityTextIndexFieldConverter(grok.MultiAdapter):
         transforms = getToolByName(self.context, 'portal_transforms')
         stream = transforms.convertTo('text/plain', html)
         return stream.getData().strip()
+
+
+if HAS_NAMEDFILE:
+    class NamedfileFieldConverter(DefaultDexterityTextIndexFieldConverter):
+        """Converts the file data of a named file using portal_transforms.
+        """
+
+        grok.provides(interfaces.IDexterityTextIndexFieldConverter)
+        grok.adapts(IDexterityContent, INamedFileField, IWidget)
+
+        def convert(self):
+            """Transforms file data to text for indexing safely.
+            """
+            storage = self.field.interface(self.context)
+            data = self.field.get(storage)
+
+            # if there is no data, do nothing
+            if not data or data.getSize() == 0:
+                return ''
+
+            # if there is no path to text/plain, do nothing
+            transforms = getToolByName(self.context, 'portal_transforms')
+            if transforms._findPath(data.contentType, 'text/plain'):
+                return ''
+
+            # convert it to text/plain
+            try:
+                return transforms.convertTo(
+                    'text/plain', data.data, mimetype=data.contentType,
+                    filename=data.filename)
+
+            except (ConflictError, KeyboardInterrupt):
+                raise
+
+            except Exception, e:
+                LOGGER.error('Error while trying to convert file contents '
+                             'to "text/plain": %s' % str(e))
