8e2a60096fd53ca65173c3a6be1fdf691ab046b9
Jonas Baumann <jone@jone.ch>
2012-03-16 08:56:11 +0100

    Remove unused import.

--- collective/dexteritytextindexer/utils.py
+++ collective/dexteritytextindexer/utils.py
@@ -1,9 +1,9 @@
-from plone.directives.form.schema import TEMP_KEY
 from collective.dexteritytextindexer.directives import SEARCHABLE_KEY
 
+
 def searchable(iface, field_name):
-    """ 
-        mark a field in existing iface as searchable 
+    """
+        mark a field in existing iface as searchable
     """
     store = iface.queryTaggedValue(SEARCHABLE_KEY)
     if store is None:
