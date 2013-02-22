f511bf0645be11af1befb0b31f3c0db5ec003f33
Jonas Baumann <jone@jone.ch>
2013-02-22 16:59:12 +0100

    Add no_longer_searchable utils function.

--- collective/dexteritytextindexer/tests/test_utils.py
+++ collective/dexteritytextindexer/tests/test_utils.py
@@ -1,4 +1,5 @@
 from collective.dexteritytextindexer.tests.helpers import get_searchable_fields
+from collective.dexteritytextindexer.utils import no_longer_searchable
 from collective.dexteritytextindexer.utils import searchable
 from plone.directives import form
 from unittest2 import TestCase
@@ -30,6 +31,23 @@ class TestUtils(TestCase):
     def test_break_when_field_does_not_exist(self):
         with self.assertRaises(AttributeError) as cm:
             searchable(IBar, u'foo')
+
+        self.assertEqual(
+            str(cm.exception),
+            'collective.dexteritytextindexer.tests.test_utils.IBar'
+            ' has no field "foo"')
+
+    def test_no_longer_searchable_removes_flag(self):
+        self.assertEquals(get_searchable_fields(IBaz), [])
+        searchable(IBaz, u'baz')
+        self.assertEquals(get_searchable_fields(IBaz), ['baz'])
+        self.assertTrue(no_longer_searchable(IBaz, 'baz'))
+        self.assertFalse(no_longer_searchable(IBaz, 'baz'))
+        self.assertEquals(get_searchable_fields(IBaz), [])
+
+    def test_no_longer_searchable_breaks_when_field_does_not_exist(self):
+        with self.assertRaises(AttributeError) as cm:
+            no_longer_searchable(IBar, u'foo')
 
         self.assertEqual(
             str(cm.exception),
