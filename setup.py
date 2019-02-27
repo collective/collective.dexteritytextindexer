05de1902ce5e65fa65dea99c0f48bfcbd37b1551
Stephan Klinger <staeff@arcor.de>
2019-02-27 19:40:57 +0100

    Update trove classifiers in setup.py

--- setup.py
+++ setup.py
@@ -24,10 +24,13 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Framework :: Plone',
         'Framework :: Plone :: 4.3',
         'Framework :: Plone :: 5.0',
         'Framework :: Plone :: 5.1',
+        'Framework :: Plone :: 5.2',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords='plone dexterity searchable text indexer',
